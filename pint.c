0bc0d336a7ed08128c214d90d5aba4b9b39bc0e3
Esther-Wagatwe <estherwagatwe@gmail.com>
2024-02-01 00:03:53 +0300

    Free memory in pint

--- pint.c
+++ pint.c
@@ -5,6 +5,9 @@ void pint_function(stack_t **stack, unsigned int line_number)
 	if (*stack == NULL)
 	{
 		fprintf(stderr, "L%u: cant pint, stack empty\n", line_number);
+		fclose(bus.file);
+		free(bus.line);
+		free_stack(*stack);
 		exit(EXIT_FAILURE);
 	}
 	
