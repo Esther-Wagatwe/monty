addda451dc538d462857cd8e12ff556b17e29b97
Esther-Wagatwe <estherwagatwe@gmail.com>
2024-02-01 17:14:13 +0300

    pint.c correction

--- pint.c
+++ pint.c
@@ -10,7 +10,7 @@ void pint_function(stack_t **stack, unsigned int line_number)
 {
 	if (*stack == NULL)
 	{
-		fprintf(stderr, "L%u: can't pint, stack empty\n", counter);
+		fprintf(stderr, "L%u: can't pint, stack empty\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 
